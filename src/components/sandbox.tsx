58e5ffc3c43aa65c78224a09d58cd4098d34be43
takeokunn <bararararatty@gmail.com>
2019-09-27 17:51:23 +0900

    Update

---
+++ src/components/sandbox.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {}
+
+interface State {}
+
+export default class Sandbox extends React.Component<Props, State> {
+  render() {
+    return (
+      <div>
+          <button className="button">fadfs</button>
+      </div>
+    )
+  }
+}
